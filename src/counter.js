6dc888e2eff5862895a8cf445bcc22fc2aa90527
Artiom <sinigur2006@gmail.com>
2025-01-28 22:40:26 -0500

    Dynamic import css

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 export const initializeCounter = (doc = globalThis.document) => {
   const countElement = doc.getElementById('count');
   const incrementButton = doc.getElementById('increment');
