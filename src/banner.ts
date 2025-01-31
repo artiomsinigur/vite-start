154e658b66dacbd94c703a7949382ddc8ee50cd2
Artiom <sinigur2006@gmail.com>
2025-01-30 19:35:23 -0500

    Add typescript

--- src/banner.ts
+++ src/banner.ts
@@ -1,6 +1,6 @@
 import styles from './banner.module.css';
 
-const bannerElement = document.querySelector('#content');
+const bannerElement = document.querySelector('#content') as HTMLDivElement;
 bannerElement.classList.add(styles.banner);
 
 export const addBanner = (text = 'Look at this banner!') => {
