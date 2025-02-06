81c399e66d32f87d14b3568f00b04761e5f18fac
Artiom <sinigur2006@gmail.com>
2025-02-06 18:33:02 -0500

    Glob Imports

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@
 import bigImage from './images/steve-after-a-workshop.jpg?w=300&format=webp&as=metadata';
 // console.log(import.meta.env); // Special object that contains the environment variables
 
-import { name } from './characters/gandorf.json';
-console.log(name);
+// import { name } from './characters/gandorf.json';
+// console.log(name);
+
+const logos = import.meta.glob('./logos/*.svg', { eager: true });
+for (const [path, url] of Object.entries(logos)) {
+  const img = document.createElement('img');
+  img.src = url.default;
+  img.width = 50;
+  document.body.appendChild(img);
+}
 
 const imageElm = document.createElement('img');
 imageElm.src = bigImage;
