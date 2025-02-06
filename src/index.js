b360bb68bc26193914e6014776f7553bd61eb7b4
Artiom <sinigur2006@gmail.com>
2025-02-06 09:11:10 -0500

    JSON named exports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 
 // import image from './android-chrome-512x512.png';
 // import image from './favicon-16x16.png';
-import bigImage from './images/steve-after-a-workshop.jpg?w=300&format=webp&as=metadata';
 
-console.log(import.meta.env); // Special object that contains the environment variables
+import bigImage from './images/steve-after-a-workshop.jpg?w=300&format=webp&as=metadata';
+// console.log(import.meta.env); // Special object that contains the environment variables
+
+import { name } from './characters/gandorf.json';
+console.log(name);
 
 const imageElm = document.createElement('img');
 imageElm.src = bigImage;
